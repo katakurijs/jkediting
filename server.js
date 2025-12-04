f52902c1c60da1ca963e9b364434a89f171a16d0
kata6999 <antizlepro@gmail.com>
2025-12-04 22:35:35 +0100

    first commita

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("trust proxy", true);
 
 app.use(
